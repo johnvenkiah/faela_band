a148f69adbcc7f0f4ff090014ba55ddeba6a68f1
johnvenkiah <contact@johnvenkiah.com>
2022-03-24 15:48:07 +0100

    add elements and styling to topnav

--- src/components/topnav/Topnav.jsx
+++ src/components/topnav/Topnav.jsx
@@ -6,8 +6,22 @@ export default function Topnav() {
         <div className="wrapper">
             <div className="left">
                 <a href="#home" className="main-logo">FAELA</a>
+                <div className="menuItem">
+                  <i className="menu-icon fa-solid fa-headphones"></i>
+                  <span>Listen</span>
+                </div>
+                <div className="menuItem">
+                  <i className="menu-icon fa-solid fa-champagne-glasses"></i>
+                  <span>Book Us</span>
+                </div>
             </div>
-            <div className="right">RIGHT SIDE</div>
+            <div className="right">
+              <div className="hamburger">
+                <span className="line-1"></span>
+                <span className="line-2"></span>
+                <span className="line-3"></span>
+              </div>
+            </div>
         </div>
     </div>
   )
