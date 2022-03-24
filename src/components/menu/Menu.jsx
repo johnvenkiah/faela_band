9d3db98c525738108547b1d8338ee23df2b1fc44
johnvenkiah <contact@johnvenkiah.com>
2022-03-24 23:29:07 +0100

    assets: add initial images to repo

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,19 +4,19 @@ export default function Menu({ openMenu, setOpenMenu }) {
   return (
     <div className={"menu " + (openMenu && "active")}>
         <ul>
-            <li>
+            <li onClick={() => setOpenMenu(false)}>
                 <a href="#home">Home</a>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(false)}>
                 <a href="#music">Music</a>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(false)}>
                 <a href="#videos">Videos</a>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(false)}>
                 <a href="#about">About</a>
             </li>
-            <li>
+            <li onClick={() => setOpenMenu(false)}>
                 <a href="#contact">Contact</a>
             </li>
         </ul>
