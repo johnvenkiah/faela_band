d8f5e8d7a5a05d7cfdc386f063d0389609a049ed
johnvenkiah <contact@johnvenkiah.com>
2022-03-26 15:57:53 +0100

    add flex to home part

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,9 @@ import "./home.scss"
 
 export default function Home() {
   return (
-    <div className="home" id="home">Home</div>
+    <div className="home" id="home">
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
   )
 }
