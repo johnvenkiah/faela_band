664a2f5f684cb90506a6760d2e1987339f384eab
johnvenkiah <contact@johnvenkiah.com>
2022-03-31 12:23:25 +0200

    music: all links for music services in place

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -35,16 +35,73 @@ export default function Music() {
           </div>
         </div>
         <div className="album">
-          <img src="/assets/cover_faela_conmigo.jpg" alt="" />
-          <h3>Con Miga (2013)</h3>
+          <img className="albumCover" src="/assets/cover_faela_conmigo.jpg" alt="Faela Por El Mundo Album Cover" />
+          <p className="p2Album">Conmigo (2013)</p>
+          <p>Play on</p>
+          <div className="playBtnContainer">
+          <a className="playBtn" href="https://open.spotify.com/album/76w4d2atxRlez2xwd3AWXg?si=O3QHNpQrQyqSzMItu56q7A" rel="noreferrer" target="_blank">
+            <i className="fa-brands fa-spotify"></i>
+            <span> Spotify</span>
+          </a>
+          <a className="playBtn" href="https://music.apple.com/se/album/conmigo/603066344?l=en" rel="noreferrer" target="_blank">
+          <img className="logoSmall" src="/assets/apple_music_logo.png" alt="Tidal Logo" />
+            <span> Apple Music</span>
+          </a>
+          <a className="playBtn" href="https://tidal.com/browse/album/88307663" rel="noreferrer" target="_blank">
+            <img className="logoSmall" src="/assets/tidal_logo.png" alt="Tidal Logo" />
+            <span> Tidal</span>
+          </a>
+          <a className="playBtn" href="https://deezer.page.link/yMYJffju3vanGa5ZA" rel="noreferrer" target="_blank">
+          <i className="fa-brands fa-deezer"></i>
+            <span> Deezer</span>
+          </a>
+          </div>
         </div>
         <div className="album">
-          <img src="/assets/cover_faela_camina.jpg" alt="" />
-          <h3>Camina (2011)</h3>
+          <img className="albumCover" src="/assets/cover_faela_camina.jpg" alt="Faela Por El Mundo Album Cover" />
+          <p className="p2Album">Camina (2011)</p>
+          <p>Play on</p>
+          <div className="playBtnContainer">
+          <a className="playBtn" href="https://open.spotify.com/album/5mBKYplAT3AxYgRaNYAzqT?si=H380_hFVSH2bSR8JHOiHHg" rel="noreferrer" target="_blank">
+            <i className="fa-brands fa-spotify"></i>
+            <span> Spotify</span>
+          </a>
+          <a className="playBtn" href="https://music.apple.com/se/album/camina/471586220?l=en" rel="noreferrer" target="_blank">
+          <img className="logoSmall" src="/assets/apple_music_logo.png" alt="Tidal Logo" />
+            <span> Apple Music</span>
+          </a>
+          <a className="playBtn" href="https://tidal.com/browse/album/88307356" rel="noreferrer" target="_blank">
+            <img className="logoSmall" src="/assets/tidal_logo.png" alt="Tidal Logo" />
+            <span> Tidal</span>
+          </a>
+          <a className="playBtn" href="https://deezer.page.link/VK9MbyHyoX5cwRVL8" rel="noreferrer" target="_blank">
+          <i className="fa-brands fa-deezer"></i>
+            <span> Deezer</span>
+          </a>
+          </div>
         </div>
         <div className="album">
-          <img src="/assets/cover_faela_faela.jpg" alt="" />
-          <h3>Faela (2011)</h3>
+          <img className="albumCover" src="/assets/cover_faela_faela.jpg" alt="Faela Por El Mundo Album Cover" />
+          <p className="p2Album">Faela (2011)</p>
+          <p>Play on</p>
+          <div className="playBtnContainer">
+          <a className="playBtn" href="https://open.spotify.com/album/2F8NRYaXrYw3Xc84UaTxrM?si=_fShO3J8Q_-qzwa7G2MRog" rel="noreferrer" target="_blank">
+            <i className="fa-brands fa-spotify"></i>
+            <span> Spotify</span>
+          </a>
+          <a className="playBtn" href="https://music.apple.com/se/album/faela/451016033?l=en" rel="noreferrer" target="_blank">
+          <img className="logoSmall" src="/assets/apple_music_logo.png" alt="Tidal Logo" />
+            <span> Apple Music</span>
+          </a>
+          <a className="playBtn" href="https://tidal.com/browse/album/88307345" rel="noreferrer" target="_blank">
+            <img className="logoSmall" src="/assets/tidal_logo.png" alt="Tidal Logo" />
+            <span> Tidal</span>
+          </a>
+          <a className="playBtn" href="https://deezer.page.link/P7YfL3bfw2YoeASb9" rel="noreferrer" target="_blank">
+          <i className="fa-brands fa-deezer"></i>
+            <span> Deezer</span>
+          </a>
+          </div>
         </div>
         {/* <div className="iframeContainer">
           <h3>Por El Mundo</h3>
