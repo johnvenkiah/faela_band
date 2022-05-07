a08deec0c206bec6be8afaefb2c763e1d62fd74f
johnvenkiah <contact@johnvenkiah.com>
2022-05-08 00:32:08 +0200

    remove redundant scss home, add provisional message to contact after clicking send

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./contact.scss"
 
 export default function Contact() {
+  const [message, setMessage] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  }
+
   return (
     <div className="contact" id="contact">
       <h1>Contact</h1>
@@ -11,7 +19,7 @@ export default function Contact() {
             Drop us a message below and we'll get back to you as soon as we can!
           </p>
         </div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="subject">Subject</label>
           <input type="text" id="subject" />
           <label htmlFor="email">Email Address</label>
@@ -19,6 +27,7 @@ export default function Contact() {
           <label htmlFor="message">Message</label>
           <textarea name="message" id="message" cols="30" rows="7"></textarea>
           <input type="submit" value="Send" />
+          {message && <span>Thanks for writing, we will reply as soon as we can!</span>}
         </form>
       </div>
     </div>
