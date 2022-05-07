16a1f36d7f8d5bb9421801afd74d8271d294a627
johnvenkiah <contact@johnvenkiah.com>
2022-05-08 01:59:49 +0200

    add email functionality contact@

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 import "./contact.scss"
 
 export default function Contact() {
+  const form = useRef();
   const [message, setMessage] = useState(false)
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setMessage(true);
+    emailjs.sendForm('gmail_jvenkiah', 'template_7gk3wq3', e.target, 'user_xdb9Fr0JUjCoYFOs4hiu4')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      })
+      .then(setMessage(true));
+      e.target.reset();
   }
 
   return (
@@ -19,16 +28,17 @@ export default function Contact() {
             Drop us a message below and we'll get back to you as soon as we can!
           </p>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form ref={form} onSubmit={sendEmail}>
           <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" />
+          <input type="text" id="subject" name="subject" className="form-control" required />
           <label htmlFor="email">Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" name="email" className="form-control" required/>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="7"></textarea>
-          <input type="submit" value="Send" />
+          <textarea name="message" id="message" cols="30" rows="7" className="form-control" required></textarea>
+          <input type="submit" value="Send" className="g-recaptcha" data-sitekey="6LcN69AfAAAAAM8mxYEGvjt3EXlXWNmpzSVF6BgG"/>
           {message && <span>Thanks for writing, we will reply as soon as we can!</span>}
         </form>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
       </div>
     </div>
   )
