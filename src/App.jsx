59e87436c850042a09a571832385f3261563a7d3
johnvenkiah <contact@johnvenkiah.com>
2022-05-16 14:11:19 +0200

    fix height bug so full height is innerHeight

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import Topnav from "./components/topnav/Topnav";
-import Home from "./components/home/Home";
-import Music from "./components/music/Music";
-import Videos from "./components/videos/Videos";
-import Live from "./components/live/Live";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import "./app.scss";
-import { useState } from "react";
-import Menu from "./components/menu/Menu";
+import Topnav from './components/topnav/Topnav';
+import Home from './components/home/Home';
+import Music from './components/music/Music';
+import Videos from './components/videos/Videos';
+import Live from './components/live/Live';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import './app.scss';
+import { useState } from 'react';
+import Menu from './components/menu/Menu';
 
 function App() {
-  const [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
+
+  function setDocHeight() {
+    document.documentElement.style.setProperty(
+      '--vh',
+      `${window.innerHeight / 100}px`
+    );
+  }
+
+  window.addEventListener('resize', setDocHeight());
+  window.addEventListener('orientationchange', setDocHeight());
+
+  setDocHeight();
+
   return (
     <div className="app">
-      <Topnav openMenu={openMenu} setOpenMenu={setOpenMenu}/>
-      <Menu openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+      <Topnav openMenu={openMenu} setOpenMenu={setOpenMenu} />
+      <Menu openMenu={openMenu} setOpenMenu={setOpenMenu} />
       <div className="sections">
-        <Home/>
-        <Music/>
-        <Videos/>
-        <Live/>
-        <About/>
-        <Contact/>
+        <Home />
+        <Music />
+        <Videos />
+        <Live />
+        <About />
+        <Contact />
       </div>
     </div>
   );
