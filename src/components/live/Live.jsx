500210a7920d2274a9a7ae22895c8be367a6f3bf
johnvenkiah <contact@johnvenkiah.com>
2022-06-03 00:07:55 +0200

    style: correct font-size, live

--- src/components/live/Live.jsx
+++ src/components/live/Live.jsx
@@ -1,4 +1,3 @@
-
 import './live.scss';
 import Calendar from '../calendar/Calendar';
 
@@ -9,37 +8,7 @@ export default function Live() {
       <div className="mainContainer">
         <h2>Catch Faela live! Click on the date for info or tickets.</h2>
         <div className="gigContainer">
-          <Calendar/>
-          {/* <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p>
-          <p className="location"><span className="timeDate">May 20th, 2022, 20:00 - </span>
-            Moriska Paviljongen, Malmö SE
-          </p> */}
+          <Calendar />
         </div>
       </div>
     </div>
