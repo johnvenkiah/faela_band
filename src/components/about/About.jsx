1b05201f27ebb164c83a574485764ad456f1e425
johnvenkiah <contact@johnvenkiah.com>
2022-06-03 18:51:17 +0200

    style: About: add small mixin for smallest screens and shorten text for mobile

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,28 +22,39 @@ export default function About() {
         <article className="textContainer">
           <section>
             <p className="aboutText">
-              Faela has built their fanbase on endless touring, great
+              Faela built their fanbase on endless touring, great songwriting
+              and explosive live shows. They are one of those bands who remind
+              us of the unifying power of music, making audiences sing and dance
+              together regardless of age, ethnicity or religion.
+              {/* Faela has built their fanbase on endless touring, great
               songwriting and explosive live shows. They are one of those bands
               who remind us of the unifying power of music, making audiences
               sing and dance together regardless of any age, ethnicity or
               religion. Now they are booking a release tour for their upcoming
-              album ”Latin Animal Beats”.
+              album ”Latin Animal Beats”. */}
             </p>
-            <p className="aboutText">
+            <p className="aboutText aboutTextTwo">
               The six musicians with roots in Argentina, England, Brazil, Spain
+              and Sweden met in Malmö, Sweden - each member with a unique
+              musical and artistic background. Out of this emerged the liveshow
+              and the sound of Faela! A ruthless mixture of Cumbia, Jazz, Rock,
+              and much more.
+              {/* The six musicians with roots in Argentina, England, Brazil, Spain
               and Sweden met in the city of Malmö in the south of Sweden. Each
               member with a unique musical and artistic background. Out of this
               emerged the liveshow and the sound of Faela! A ruthless mixture of
-              Cumbia, Jazz, Rock, and just about every latin genre there is.
+              Cumbia, Jazz, Rock, and just about every latin genre there is. */}
             </p>
             <p className="aboutText">
-              The band was formed in 2007 in Malmö, Sweden. Since then they have
+              The band formed in 2007 in Malmö, Sweden. Since then they have
+              released 4 albums and 1 EP, played nearly 1000 shows at festivals
+              and in clubs all over Europe and in South America. The ever
+              growing fan base show that Faela truly is irresistible!
+              {/* The band was formed in 2007 in Malmö, Sweden. Since then they have
               released 4 albums and 1 EP. They have played nearly 1000 shows on
               festivals and clubs all over Europe and in South America. The ever
-              growing fan base show that Faela truly is irresistible!
+              growing fan base show that Faela truly is irresistible! */}
             </p>
-          </section>
-          <section>
             <div className="aboutText">
               <h3>Members:</h3>
               <div className="leftCol">
