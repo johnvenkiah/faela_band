ee30370ecb30d18c3e17b8dc001065d9bc20b3dd
johnvenkiah <contact@johnvenkiah.com>
2022-06-05 00:27:15 +0200

    try z-index and attrs for iframe for correct layering of menu on top of iframes

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,57 +1,3 @@
-// import './videos.scss';
-
-// export default function Videos() {
-//   const video_urls = [
-//     {
-//       title: 'Perro Fiel',
-//       url: 'zOrhSgxerjs',
-//       description: "Faela's new song with footage from Boa Vista, Cap Verde",
-//     },
-//     {
-//       title: 'Samuelito',
-//       url: 't7GJXdiU2KI',
-//       description: "A tribute to Faela's old friend in Switzerland",
-//     },
-//     {
-//       title: 'Efige Efige',
-//       url: 'aTp9XAqkFS0',
-//       description:
-//         "Faela's take on a Greek tune by Stelios Kazantzidis from 1980",
-//     },
-//     {
-//       title: 'Live from Hildesheim',
-//       url: 'Ywgn4aaAXWU?start=633',
-//       description: 'A live concert streamed from Germany, 2021',
-//     },
-//   ];
-//   return (
-//     <div className="videos" id="videos">
-//       <h1>Videos</h1>
-//       <div className="videoContainer">
-//         {video_urls.map((item, i) => (
-//           <>
-//             <h3>{item.title}</h3>
-//             <div className="videoWrapper">
-//               <div className="textWrapper">
-//                 <iframe
-//                   src={`https://www.youtube.com/embed/${item.url}`}
-//                   title={`YouTube video - ${item.title}`}
-//                   frameBorder="0"
-//                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//                   allowFullScreen
-//                 ></iframe>
-//               </div>
-//               <div className="textWrapper">
-//                 <p>{item.description}</p>
-//               </div>
-//             </div>
-//           </>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
 import './videos.scss';
 
 export default function Videos() {
@@ -88,11 +34,12 @@ export default function Videos() {
               <h3>{item.title}</h3>
               <div className="textWrapper">
                 <iframe
-                  src={`https://www.youtube.com/embed/${item.url}`}
+                  src={`https://www.youtube.com/embed/${item.url}?wmode=transparent`}
                   title={`YouTube video - ${item.title}`}
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
+                  wmode="Opaque"
                 ></iframe>
                 <p>{item.description}</p>
               </div>
