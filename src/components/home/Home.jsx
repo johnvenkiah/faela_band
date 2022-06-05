6e7c58cd8c75f35ef9d31e13ce565a8b106120f6
johnvenkiah <contact@johnvenkiah.com>
2022-06-05 14:18:08 +0200

    small corrections home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,18 +5,24 @@ export default function Home() {
   return (
     <div className="home" id="home">
       <div className="left">
-        <div className="imgContainer">
+        <a
+          className="imgContainer"
+          href="https://www.youtube.com/watch?v=zOrhSgxerjs"
+          rel="noreferrer"
+          target="_blank"
+        >
           <img
             src={process.env.PUBLIC_URL + '/assets/cover_faela_lab.jpg'}
             alt="CD-cover for album Latin Animal Beats"
           />
-        </div>
+          <p>Prelisten on YouTube</p>
+        </a>
       </div>
       <div className="right">
         <div className="homeRightContainer">
           <h1>Faela's new album</h1>
           <h2>Latin Animal Beats</h2>
-          <h3>Out Summer 2022</h3>
+          <h3>Out Soon</h3>
           <Link to="/music" className="btnMain">
             <p>Continue to music</p>
             <i className="fa-solid fa-chevron-down"></i>
