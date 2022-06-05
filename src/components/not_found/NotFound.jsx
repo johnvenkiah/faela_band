a974bd41aeae29ea6a7b189bd174d1edfb7da1fb
johnvenkiah <contact@johnvenkiah.com>
2022-06-05 03:25:57 +0200

    style 404, remove comments

--- src/components/not_found/NotFound.jsx
+++ src/components/not_found/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="notFoundContainer">
       <div className="wrapper">
-        <h1>PAGE NOT FOUND</h1>
+        <h1>PAGE NOT FOUND =(</h1>
         <div className="linkPara">
           <p className="">Navigate back</p>
           <Link to="/">Home</Link>
