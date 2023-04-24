9aa8c2b642131f4cb13fdaab1e9be3a2deb7cb18
johnvenkiah <contact@johnvenkiah.com>
2023-04-24 23:59:37 +0200

    revert to when it worked without netlify

---
+++ .netlify/functions-serve/.unzipped/server/server.js
@@ -0,0 +1,47 @@
+// const app = require('../server');
+// const cors = require('cors');
+
+// exports.handler = async (event, context) => {
+//   const { httpMethod, path } = event;
+//   app.use(cors());
+//   if (httpMethod !== 'GET' || !path.startsWith('/api/calendar/events')) {
+//     return {
+//       statusCode: 404,
+//       body: JSON.stringify({
+//         error: 'Not found',
+//       }),
+//     };
+//   }
+
+//   try {
+//     const req = {
+//       method: httpMethod,
+//       url: path,
+//       headers: event.headers,
+//     };
+//     const res = {
+//       json: (data) => ({
+//         statusCode: 200,
+//         // body: data,
+//         body: JSON.stringify(data),
+//       }),
+//       status: (statusCode) => ({
+//         json: (data) => ({
+//           statusCode,
+//           // body: data,
+//           body: JSON.stringify(data),
+//         }),
+//       }),
+//     };
+//     const response = await app(req, res);
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({
+//         error: 'Server error',
+//       }),
+//     };
+//   }
+// };
